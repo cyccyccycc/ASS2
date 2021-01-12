d0ec5e05a40b37062203da83a66b651ae8611457
YiCheng Cai <20091598@mail.wit.ie>
2021-01-12 15:13:05 +0000

    Week 9 Included Mongoose

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import moviesRouter from './api/movies';
 import bodyParser from 'body-parser';
+import './db';
+import {loadUsers} from './seedData'
 
 dotenv.config();
 const errHandler = (err, req, res, next) => {
@@ -12,6 +14,10 @@ const errHandler = (err, req, res, next) => {
     }
     res.status(500).send(`Hey!! You caught the error 👍👍, ${err.stack} `);
   };
+
+  if (process.env.SEED_DB) {
+    loadUsers();
+  }
 const app = express();
 
 const port = process.env.PORT;
